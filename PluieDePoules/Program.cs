832f962c3075d55463b387ad56d88cff52aa1c05
Julien-Turcotte <6315237@cegepoutaouais.qc.ca>
2026-08-28 01:43:27 -0400

    base des poules

--- PluieDePoules/Program.cs
+++ PluieDePoules/Program.cs
@@ -1,38 +1,69 @@
-﻿class Program
+﻿using PluieDePoules;
+
+class Program
 {
     static void Main(string[] args)
     {
-        // Point d'entrée du programme, appelez vos fonctions ici.
+
+        Random random = new Random();
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        List<Poule> poules = new List<Poule>();
+
+        while (true)
+        {
+            Poule? nouvellePoule = CreerPoule(random.Next(1, Console.WindowWidth), random.Next(0, 100));
+            if (nouvellePoule != null)
+            {
+                poules.Add(nouvellePoule);
+            }
+
+            foreach (Poule p in poules.ToList())
+            {
+                if (CheckSol(p))
+                {
+                    poules.Remove(p);
+                }
+            }
+            UpdateStatus(poules);
+            Thread.Sleep(100);
+        }
     }
 
+    /// <summary>
+    /// A 20% de chance de créer une poule
+    /// </summary>
+    /// <param name="x">la position horizontale de la poule</param>
+    /// <param name="random">un nombre qui determine si la poule va apparaite</param>
+    /// <returns>La poule si créer</returns>
+    private static Poule? CreerPoule(int x, int random)
+    {
+        if (random < 20)
+        {
+            Poule p = new Poule(x);
+            return p;
+        }
+        return null;
+    }
 
-    // TODO: Créez des fonctions et documentez les (entrez /// pour générer la structure de documentation de la fonction).
-
-    /*
-        Quelques notions à utiliser
-
-        Hauteur de la fenêtre console en nombre de lignes : Console.WindowHeight
-        exemple :
-        int sol = Console.WindowHeight - 2;
-
-        Positionner le curseur sur une position spécifique dans la console:
-             x : la colonne(de gauche à droite, commence à 0).
-             y : la ligne(de haut en bas, commence à 0).
-        Console.SetCursorPosition(x, y);
-
-        Configuration pour afficher les emojis
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-        Random random = new Random(); // Une seule instance de Random pour éviter les répétitions
-        random.Next(posMin, posMax + 1);
-
-        Temps de pause (en millisecondes)
-        Thread.Sleep(50);
-    
-        Effacer la console 
+    /// <summary>
+    /// reaffiche les poules une case plus bas
+    /// </summary>
+    /// <param name="poules">La liste de poules sur l'écran</param>
+    private static void UpdateStatus(List<Poule> poules)
+    {
         Console.Clear();
-     */
+        foreach(Poule p in poules)
+        {
+            p.Tomber();
+            Console.SetCursorPosition(p.x, p.y);
+            Console.Write("🐔");
+        }
 
-
+    }
+    private static bool CheckSol(Poule poule)
+    {
+        return poule.y >= Console.WindowHeight - 2;
+    }
 
 }
---
+++ PluieDePoules/poule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluieDePoules
+{
+    public class Poule
+    {
+        public int x;
+        public int y;
+
+        public Poule(int x, int y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Tomber()
+        {
+            this.y = y + 1;
+        }
+    }
+}
